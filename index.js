4a6f344bf47f5b5908b65b9f4e8e707eb19514c7
Deepakraja03 <12306deepakraja@gmail.com>
2024-03-16 15:00:32 +0530

    code updated for version 2.0.1

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-
 const express = require('express');
-const createAuthMiddleware = require('dynamic-auth');
-const createMongoDBConnection = require('dynamic-auth');
+const {createMongoDBConnection, createAuthMiddleware} = require('dynamic-auth');
 require('dotenv').config();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-if (createMongoDBConnection(process.env.MONGODB_URI)) {
-    // Middleware
-    app.use(express.json());
+createMongoDBConnection(process.env.MONGODB_URI,PORT);
 
-    // Import Mongoose model for user from another folder
-    const User = require('./models/user');
+ // Middleware
+ const app = express();
+ app.use(express.json());
 
-    // Define fields for user data
-    const userDataFields = ['firstname', 'lastname', 'email', 'address', 'city', 'country'];
+ // Import Mongoose model for user from another folder
+ const User = require('./models/user');
 
-    // Authentication routes
-    app.use('/auth', createAuthMiddleware({ jwtSecret: process.env.JWT_SECRET, UserModel: User, userDataFields }));
+ // Define fields for user data
+ const userDataFields = ['firstname', 'lastname', 'email', 'address', 'city', 'country'];
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-    console.log('Database connected successfully');
-    
-  } else {
-    console.log('Failed to connect to the database');
-  }
+ // Authentication routes
+ app.use('/auth', createAuthMiddleware({ jwtSecret: process.env.JWT_SECRET, UserModel: User, userDataFields }));
+
